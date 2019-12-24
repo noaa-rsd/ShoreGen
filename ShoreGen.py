61ada91ab25e35c55c95ce75af2665767a72d9d8
forkozi <nick.forfinski@gmail.com>
2019-12-24 18:16:03 -0500

    cleaned up use of out_dir

--- ShoreGen.py
+++ ShoreGen.py
@@ -4,15 +4,13 @@ import numpy as np
 from shapely.geometry import LineString
 from shapely import affinity
 import geopandas as gpd
-import rasterio
+import rasterio as rio
 from rasterio import Affine, merge, plot
 from rasterio.enums import Resampling
 from rasterio.crs import CRS
 from skimage import io, measure, transform
 from skimage.morphology import dilation, disk
-
 from sklearn import cluster
-
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import colors
@@ -34,72 +32,56 @@ def set_env_vars(env_name):
     os.environ["GDAL_DATA"] = str(gdal_data_path)
 
 
-class ImageTile:
+class ShorelineRasterTile:
 
-    def __init__(self):
-        self.tifs = []
-        self.tifs_rgb = []
-        self.out_meta = None
+    def __init__(self, srt_path):
+        self.srt_path = srt_path
+        self.img = io.imread(srt_path)
+        self.profile = self.get_profile()
+        self.rescale_factor = None
+        self.red = self.img[:, :, 0]
+        self.green = self.img[:, :, 1]
+        self.blue = self.img[:, :, 2]
+        self.nir = self.img[:, :, 3]
 
-    def get_bands(self):
-        pass
+    def get_profile(self):
+        src = rio.open(self.srt_path)
+        return src.profile
 
-    def calc_index(self, dem, water_dir):
-        img = io.imread(dem)
-            
+    def update_profile(self):
+        self.profile.update({
+            'count': 1,
+            'height': rescaled_height, 
+            'width': rescaled_width, 
+            #'dtype': 'float64',
+            'nodata': 1, 
+            'transform': self.get_resized_transform()})
 
-        #red =  img[:, :, 0]
-        #green = img[:, :, 1]
-        #blue = img[:, :, 2]
-        #nir = img[:, :, 3]
-            
-        #ndwi = (green - nir) / (green + nir)
-        #ndwi = np.where(np.isfinite(ndwi), ndwi, -1)
-        #ndwi = np.expand_dims(ndwi, axis=2)
+    def get_resized_transform(self):
+        t = self.profile['transform']
+        return  Affine(t.a / rescale_factor, t.b, t.c,
+                       t.d, t.e / rescale_factor, t.f)
 
-        #ndvi = (red - nir) / (red + nir)
-        #ndvi = np.where(np.isfinite(ndvi), ndvi, -1)
-        #ndvi = np.expand_dims(ndvi, axis=2)
+    def calc_ndvi(self):
+        ndvi = (self.red - self.nir) / (self.red + self.nir)
+        ndvi = np.where(np.isfinite(ndvi), ndvi, -1)
+        ndvi = np.expand_dims(ndvi, axis=2)
+        return np.where(ndwi > 0, 0, 1).astype(np.uint8)
 
-        return index_raster
+    def resize(self, srt, rescale_factor=0.25):
+        self.rescale_factor = rescale_factor
+        rescaled_height = int(srt.shape[0] * rescale_factor)
+        rescaled_width = int(srt.shape[1] * rescale_factor)
+        rescaled_shape = (rescaled_height, rescaled_width)
+        return transform.resize(srt, rescaled_shape, 
+                                anti_aliasing=True).astype('uint8')
 
-    def resize_boolean_raster(self, img):
-        rescale_factor = 0.25
-        img_s1_height = int(img.shape[0] * rescale_factor)
-        img_s1_width = int(img.shape[1] * rescale_factor)
-        output_shape = (1, img_s1_height, img_s1_width)
-        print(output_shape)
-        img_s1_ski = transform.resize(img, output_shape[1:], anti_aliasing=True)
+    def export(self, resized_srt, export_path):
+        self.update_profile()
+        with rio.open(export_path, 'w', n_bits=1, **self.profile) as dst:
+            dst.write(resized_srt, 1)
 
-        src = rasterio.open(dem)
-            
-        red =  img_s1_ski[:,:,0]
-        green = img_s1_ski[:,:,1]
-        blue = img_s1_ski[:,:,2]
-        nir = img_s1_ski[:,:,3]
-            
-        ndwi = (green - nir) / (green + nir)
-        ndwi = np.where(np.isfinite(ndwi), ndwi, -1)            
-        water = np.where(ndwi > 0, 0, 1).astype(np.uint8)
-
-        meta = src.profile
-        t = meta['transform']
-        resize_offset = 1 / rescale_factor
-        t_affine = Affine(t.a / rescale_factor, t.b, t.c, 
-                            t.d, t.e / rescale_factor, t.f)
-
-        meta.update(count=1, 
-                    height=x, 
-                    width=y, 
-                    #dtype='float64',
-                    nodata=1,
-                    transform=t_affine)
-
-        ndwi_path = water_dir / '{}_Kmeans.tif'.format(dem.stem)
-        with rasterio.open(ndwi_path, 'w', n_bits=1, **meta) as dst:
-            dst.write(water, 1)
-
-    def classify_tif(self):
+    def k_means(self):
         x, y, z = img_s1_ski.shape
         img_2d = img_s1_ski.reshape(x*y, z)
 
@@ -117,187 +99,113 @@ class ImageTile:
         plt.imshow(k_means_img)
         plt.show()
 
-    def threshold_index_raster(self):
-        pass
-
-    def generalize():
-        img_s0_path = Path(r'C:\QAQC_contract\WATER.tif')
-        img_s1_path = Path(r'C:\QAQC_contract\WATER_s1.tif')
-
-        print('reading image...')
-        img = io.imread(img_s0_path)
-        img_rasterio = rasterio.open(img_s0_path)
-
-        rescale_factor = 0.1
-    
-        epsg = 26905  # NAD83/UTM Zone 5N
-        epsg = 26914  # NAD83/UTM Zone 14N
-        crs = CRS.from_epsg(epsg)
-
-        simplify = 20
-        smoothing = None
-
-        n = 4250
-        e = 16500
-        s = 6000
-        w = 14500
-
-        n_r = n * rescale_factor
-        e_r = e * rescale_factor
-        s_r = s * rescale_factor
-        w_r = w * rescale_factor
-
-        img_s1_height = int(img.shape[0] * rescale_factor)
-        img_s1_width = int(img.shape[1] * rescale_factor)
-
-        output_shape_SKI = (img_s1_height, img_s1_width, 4)
-        output_shape_RAS = (1, img_s1_height, img_s1_width)
-        print('output shape (Skimage):  {}'.format(output_shape_SKI))
-        print('output shape (Rasterio):  {}'.format(output_shape_RAS))
-
-        print('rescaling image ({})...'.format(rescale_factor))
-        img_s1_ski = transform.resize(img, output_shape_SKI, anti_aliasing=True).astype('uint8')
-
-        print('dilating skimage resized LNDARE...')
-        disk_size = 1
-        #img_s1_ski = dilation(img_s1_ski, disk(disk_size))
-
-        print('resampling rasterio img...')
-        with rasterio.open(str(img_s0_path)) as f:
-            img_s1_rio = f.read(out_shape=output_shape_RAS,
-                                resampling=Resampling.average)
-
-        profile = img_rasterio.profile
-        t = img_rasterio.meta['transform']
-    
-        resize_offset = 1 / rescale_factor
-
-        t_affine = Affine(t.a / rescale_factor, t.b, t.c, 
-                          t.d, t.e / rescale_factor, t.f)
-
-        shapely_affine = (t.a / rescale_factor, t.b, t.d,
-                          t.e / rescale_factor, t.c, t.f)
-    
-        profile.update(
-            height=img_s1_height,
-            width=img_s1_width,
-            count=1,
-            crs=crs.wkt,
-            dtype='uint8',
-            transform=t_affine)
-
-        print('writing {}...'.format(img_s1_path))
-        with rasterio.open(str(img_s1_path), 'w', **profile) as dst:
-            dst.write(img_s1_ski, 4)
-
-        img_s1_rio = np.ma.array(img_s1_rio.squeeze(), mask=(img_s1_rio == 1))
-
-        print('creating contours...')
-        contours_SKI = measure.find_contours(img_s1_ski, 0.0)
-        contours_RIO = measure.find_contours(img_s1_rio, 0.0)
-        
-        contours_list_SKI = []
-        for c in contours_SKI:
-            c[:, [0, 1]] = c[:, [1, 0]]
-            c_trans = affinity.affine_transform(LineString(c), shapely_affine)
-            contours_list_SKI.append(c_trans)
-
-        contours_list_RIO = []
-        for c in contours_RIO:
-            c[:, [0, 1]] = c[:, [1, 0]]
-            c_trans = affinity.affine_transform(LineString(c), shapely_affine)
-            contours_list_RIO.append(c_trans)
-
-        contour_gpkg = Path(r'C:\QAQC_contract\WATER.gpkg')
-        rescale_factor_int = int(1 / rescale_factor)
-
-        gdf = gpd.GeoDataFrame(geometry=contours_list_SKI, crs=crs.to_dict())
-        gdf.geometry = gdf.geometry.simplify(tolerance=simplify, preserve_topology=True)
-        lyr = 'SKI_sf{}'.format(rescale_factor_int)
-        gdf.to_file(str(contour_gpkg), layer=lyr, driver='GPKG')
-
-        gdf = gpd.GeoDataFrame(geometry=contours_list_RIO, crs=crs.to_dict())
-        lyr = 'RIO_sf{}'.format(rescale_factor_int)
-        gdf.to_file(str(contour_gpkg), layer=lyr, driver='GPKG')
-
-    def get_classified_dems(self, dem_dir):
-        print('retreiving individual QL DEMs...')
-        for dem in list(dem_dir.glob('*_Kmeans.tif')):
-            src = rasterio.open(dem)
-            self.tifs.append(src)        
-            self.out_meta = src.meta.copy()  # uses last src made
-
-    def mosaic(self, water_dir):
-        water_path = water_dir / 'WATER.tif'
-        self.get_classified_dems(water_dir)
-        if self.tifs:
-            print('generating {}...'.format(water_path))
-            mosaic, out_trans = merge.merge(self.tifs)
-            self.out_meta.update({
-                'driver': "GTiff",
-                'height': mosaic.shape[1],
-                'width': mosaic.shape[2],
-                'transform': out_trans})
-            with rasterio.open(water_path, 'w', **self.out_meta) as dest:
-                dest.write(mosaic)
-        else:
-            print('No DEM tiles were generated.')
-
 
 class ShorelineRaster:
 
-    def __init__(self):
-        pass
+    def __init__(self, img_dir):
+        self.srt_sources = self.get_shoreline_raster_tiles(img_dir)
+        self.out_profile = self.srt_sources[-1].profile.copy()
+        self.mosaic_trans = None
+
+    def get_shoreline_raster_tiles(self, img_dir):
+        shoreline_raster_tiles = []
+        print('retreiving shoreline raster tiles...')
+        for i, srt in enumerate(list(img_dir.glob('*_SRT.tif'))):
+            src = rio.open(srt)
+            shoreline_raster_tiles.append(src)        
+        return shoreline_raster_tiles
+
+    def update_profile(self):
+        self.out_profile.update({
+            'driver': "GTiff",
+            'height': mosaic.shape[1],
+            'width': mosaic.shape[2],
+            'transform': self.mosaic_transform})
+
+    def mosaic(self, water_dir):
+        mosaic, self.mosaic_transform = merge.merge(self.srt_sources)
+    
+    def dilate(self, export_path, disk_size=1):
+        print('dilating shoreline raster...')
+        dilated_mosaic = dilation(mosaic, disk(disk_size))
+        if export_path:
+            self.update_profile()
+            with rio.open(str(export_path), 'w', **self.out_profile) as dst:
+                dst.write(dilated_mosaic, 4)
+
+    def to_vector(self, dilated_mosaic):
+        contours = measure.find_contours(dilated_mosaic, 0.0)
+        t = self.mosaic_transform
+        shapely_affine = (t.a, t.b, t.d, t.e, t.c, t.f)
+        contours_list = []
+        for c in contours:
+            c[:, [0, 1]] = c[:, [1, 0]]
+            c_trans = affinity.affine_transform(LineString(c), shapely_affine)
+            contours_list.append(c_trans)
+        return contours_list
 
 
 class Shoreline:
 
-    def __init__(self):
-        pass
+    def __init__(self, contours_list, epsg):
+        crs = CRS.from_epsg(epsg)
+        self.gdf = gpd.GeoDataFrame(geometry=contours_list, crs=crs.to_dict())
 
-    def make_pretty():
-        pass
+    def make_pretty(self, simplify=20, smoothing=None):
+        self.gdf.geometry = self.gdf.geometry.simplify(tolerance=simplify, 
+                                                       preserve_topology=True)
+
+    def export(self, gpkg, lyr):
+        self.gdf.to_file(str(contour_gpkg), layer=lyr, driver='GPKG')
+
 
 def main():
 
     tif_dir = Path(r'\\ngs-s-rsd\Lidar_Contract00\TX1803\Imagery\ortho\UTM14')
-    water_dir = Path(r'C:\QAQC_contract') 
     tif_paths = list(tif_dir.glob('*.tif'))
-    num_tifs = len(list(tif_paths))
-    water_path = water_dir / 'ShorelineMOSAIC.tif'
+    num_tifs = len(tif_paths)
+    out_dir = Path(r'C:\QAQC_contract') 
 
-    img_tiles = []
+    epsgs = {
+        'NAD83/UTM Zone 5N': 26905,
+        'NAD83/UTM Zone 14N': 26914,
+        }
 
-    for i, dem in enumerate(tif_paths, 1):
+    for i, tif_path in enumerate(tif_paths, 1):
 
         print('processing shoreline image tile {} if {}...'.format(i, num_tifs))
-        img_tile = ImageTile(dem)
-        img_tiles.append(img_tile)
-        
-        # get  tif bands
-        bands = img_tile.get_bands(dem)
+        srt = ShorelineRasterTile(tif_path)
 
         # calc index raster
-        index_raster = img_tile.calc_index(dem)
-
-        # threshold index raster
-        img_tile.threshold_index_raster(index_raster)
+        ndvi_raster = srt.calc_ndvi()
 
         # resize boolean raster
-        img_tile.resize_boolean_raster()
+        resized_srt = srt.resize(ndvi_raster)
+
+        # export resized boolean shoreline raster (SR)
+        export_path = out_dir / '{}_SRT.tif'.format(self.srt_path.stem)
+        srt.export(resized_srt, export_path)
 
     # mosaic shoreline boolean rasters
     shoreline_raster = ShorelineRaster()
-    shoreline_raster.mosaic(img_tiles)
+    shoreline_raster.mosaic()
     
     # generalize boolean raster
-    shoreline_raster.generalize()
+    export_path = out_dir / 'ShorelineRaster.tif'
+    dilated_mosaic = shoreline_raster.dilate(export_path)
 
     # extract vector shoreline
-    shoreline = Shoreline(shoreline_raster.to_vector())
+    contour_geomety = shoreline_raster.to_vector(dilated_mosaic)
+    crs_epsg = epsgs['NAD83/UTM Zone 14N']
+    shoreline = Shoreline(contour_geomety, crs_epsg)
 
-    # generalized contours
+    # carogrpahically generalize contours
     shoreline.make_pretty()
+
+    # export cartographically generalized shoreline
+    contour_gpkg = out_dir / 'Shoreline.gpkg'
+    lyr = 'NDVI'
+    shoreline.export()
 
 
 if __name__ == '__main__':
