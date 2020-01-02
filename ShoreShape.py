be80f0734fe5ae0c1cd8dc063f22cde521328acd
Nick Forfinski-Sarkozi <nick.forfinski@gmail.com>
2020-01-02 15:14:59 -0500

    created PDS (Photo-Derived Shoreline) tool; created SchemaAttribute class

--- ShoreShape.py
+++ ShoreShape.py
@@ -1,15 +1,54 @@
+import json
 import geopandas as gpd
 from pathlib import Path
 
-shp_path = Path(r'\\ngs-s-rsd\Lidar_Contract00\TX1803\Imagery\ortho\shp\TX1803-UTM14-NDWI.shp')
 
-print('reading {}...'.format(shp_path))
-gdf = gpd.read_file(str(shp_path))
-print(gdf)
+class SchemaAttribute:
 
-gpkg_path = Path(r'\\ngs-s-rsd\Lidar_Contract00\TX1803\Imagery\ortho\shp\TX1803.gpkg')
-layer = 'NDWI_Contour'
-print('writing gpkg...')
-gdf.to_file(gpkg_path, layer=layer, driver='GPKG')
+    def __init__(self, label, **kwargs):
+        self.attribute_label = label
+        self.__dict__.update(kwargs)
+
+    def __str__(self):
+        return json.dumps(self.__dict__, indent=1)
 
 
+class Shoreline:
+
+    def __init__(self):
+        pass
+
+
+def create_schema():
+    schema_path = Path(r'Z:\ShoreGen\ShoreGen_noaa-rsd\shoreline_schema.json')
+    with open(schema_path, 'r') as j:
+        schema_definition = json.load(j)
+
+    schema = {}
+    for k, v in schema_definition.items():
+        schema[k] = SchemaAttribute(k, **v)
+
+    return schema
+
+
+def main():
+    schema = create_schema()
+    print(schema)
+
+    proj_dir = Path(r'\\ngs-s-rsd\Lidar_Contract00\TX1803\Imagery\ortho\shp')
+    shp_dir = proj_dir / 'tiles'
+
+    shps = list(shp_dir.glob('*.shp'))
+    num_shps = len(shps)
+    for i, shp in enumerate(shps, 1):
+        print('processing {} ({} of {})...'.format(shp.name, i, num_shps))
+        gdf = gpd.read_file(str(shp))
+
+        if not gdf.empty:
+            gpkg_path =  proj_dir / 'TX1803.gpkg'
+            layer = 'shp_{}'.format(shp.stem)
+            gdf.to_file(gpkg_path, layer=layer, driver='GPKG')
+
+
+if __name__ == '__main__':
+    main()
